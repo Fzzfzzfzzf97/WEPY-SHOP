63984d57307b3b05812f1acbbaa8d430db4bffc8
littlecat <5234847+littlecat0803@user.noreply.gitee.com>
2019-11-28 21:28:07 +0800

    商品列表完成

---
+++ src/mixins/goods_list.js
@@ -0,0 +1,54 @@
+import wepy from 'wepy'
+export default class extends wepy.mixin {
+    data = {
+        // 查询关键词
+        query: '',
+        // 商品分类的Id
+        cid: '',
+        // 页码值
+        pagenum: 1,
+        // 每页显示多少条数据
+        pagesize: 10,
+        goodslist: [],
+        isOver: false,
+        isloading: false
+    }
+    methods = {
+
+        }
+        // 获取商品列表数据
+    async getGoodsList(cb) {
+        // 即将发起请求时，将 isloading 重置为 true
+        this.isloading = true
+        const { data: res } = await wepy.get('/goods/search', {
+            query: this.query,
+            cid: this.cid,
+            pagenum: this.pagenum,
+            pagesize: this.pagesize
+        })
+
+        if (res.meta.status !== 200) {
+            return wepy.baseToast()
+        }
+
+        this.goodslist = [...this.goodslist, ...res.message.goods]
+        this.total = res.message.total
+            // 当数据请求完成后，将 isloading 重置为 false
+        this.isloading = false
+        this.$apply()
+    }
+    onLoad(options) {
+        this.query = options.query || ''
+        this.cid = options.cid || ''
+        this.getGoodsList()
+    }
+    onReachBottom() {
+        if (this.pagenum * this.pagesize >= this.total) {
+            this.isOver = !this.isOver;
+            return
+        }
+        console.log('触底了')
+        this.pagenum++
+            this.getGoodsList()
+    }
+}
